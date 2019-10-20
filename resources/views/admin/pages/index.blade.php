40be403ee09d7da9752c2da50c79fff5527c1fc9
Rafsan <kalilinux609@gmail.com>
2019-10-20 21:21:39 +0600

    admin_css

--- resources/views/admin/pages/index.blade.php
+++ resources/views/admin/pages/index.blade.php
@@ -7,15 +7,15 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <title>Star Admin Free Bootstrap-4 Admin Dashboard Template</title>
   <!-- plugins:css -->
-  <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-  <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+  <link rel="stylesheet" href="{{  asset ('css/admin/node_modules/mdi/css/materialdesignicons.min.css') }}">
+  <link rel="stylesheet" href="{{ asset ('css/admin/node_modules/simple-line-icons/css/simple-line-icons.css')}}">
   <!-- endinject -->
   <!-- plugin css for this page -->
   <!-- End plugin css for this page -->
   <!-- inject:css -->
-  <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+  <link rel="stylesheet" href="{{ asset ('css/admin/css/style.css')}}">
   <!-- endinject -->
-  <link rel="shortcut icon" href="{{ asset('css/app.css') }}" />
+  <link rel="shortcut icon" href="{{  asset ('images/favicon.png')}}" />
 </head>
 
 <body>
